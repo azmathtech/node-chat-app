3ffee64f7415bdf004a0fb2a253b8eee0de1460d
Michael Evans <azmathtech@gmail.com>
2018-11-14 14:01:17 -0700

    Add form for messages and show incoming messages in browser

--- public/js/indexold.js
+++ public/js/indexold.js
@@ -2,20 +2,43 @@ var socket = io();
 
 socket.on('connect', function() {
   console.log('Connected to server');
+
+  // socket.emit('createEmail', {
+  //   to: 'jen@example.com',
+  //   text: 'Hey this is mike.'
+  // });
+  // socket.emit('createMessage', {
+  //   from: 'Andrew',
+  //   text: 'Response from client'
+  // });
 });
 
 socket.on('disconnect', function() {
   console.log('Disconnected from server');
 });
 
+// socket.on('newEmail', function(email) {
+//   console.log('New Email', email);
+// });
 socket.on('newMessage', function(message) {
-  console.log('newMessage', message);
+  console.log('New message', message);
 
   var li = jQuery('<li></li>');
   li.text(`${message.from}:  ${message.text}`);
 
   jQuery('#messages').append(li);
 });
+
+// socket.emit(
+//   'createMessage',
+//   {
+//     from: 'Frank',
+//     text: 'Hi'
+//   },
+//   function(data) {
+//     console.log('Got it', data);
+//   }
+// );
 
 jQuery('#message-form').on('submit', function(e) {
   e.preventDefault();
--- server/server.js
+++ server/server.js
@@ -8,9 +8,6 @@ const socketIO = require('socket.io');
 const { generateMessage } = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT;
-// console.log(__dirname + '/../public');
-// console.log(publicPath);
-
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
@@ -20,16 +17,20 @@ app.use(express.static(publicPath)); //middleware
 io.on('connection', socket => {
   console.log('New user connected');
 
-  io.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+  socket.emit(
+    'newMessage',
+    generateMessage('Admin', 'Welcome to the chat app')
+  );
 
   socket.broadcast.emit(
     'newMessage',
     generateMessage('Admin', 'New user joined')
   );
 
-  socket.on('createMessage', message => {
+  socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
     io.emit('newMessage', generateMessage(message.from, message.text));
+    callback('This is from the server.');
     // socket.broadcast.emit('newMessage', {
     //   from: message.from,
     //   text: message.text,
