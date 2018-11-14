ede212af0a831a4ac67b86228f079f453fddca58
Michael Evans <azmathtech@gmail.com>
2018-11-14 11:42:06 -0700

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -30,6 +30,17 @@ io.on('connection', socket => {
   //   createdAt: 1234
   // });
 
+  io.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined the chat',
+    createdAt: new Date().getTime()
+  });
+
   socket.on('createMessage', message => {
     console.log('createMessage', message);
     io.emit('newMessage', {
@@ -37,6 +48,11 @@ io.on('connection', socket => {
       text: message.text,
       createdAt: new Date().getTime()
     });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   // socket.on('createEmail', newEmail => {
